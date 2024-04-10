c3d364dc5f179122d408576865cfb47cfe9d7b20
simoncns <simoncns@dukes.jmu.edu>
2024-04-10 02:06:25 -0400

    Front-End Update

--- weather/script.js
+++ weather/script.js
@@ -1,6 +1,13 @@
-const form = document.getElementById('form');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const zipCode = document.getElementById('zip').value;
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const zipCode = document.getElementById('zip').value;
+        console.log(zipCode);
+
+        const url = 'http://localhost:5000/weather?zip=' + encodeURIComponent(zipCode);
+
+        console.log(url);
+    });
 });
